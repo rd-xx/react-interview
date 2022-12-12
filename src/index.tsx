68183524784fd1b0a7209a8fa581ede8ce8ffbdd
rdx <reydux95@gmail.com>
2022-12-12 21:01:18 +0100

    install MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import React from 'react';
 import App from './App';
 import './index.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
